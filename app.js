a1a99df68391e0d0a5306194db16278b9189992c
YOODOOYEON <endus0713@naver.com>
2017-11-15 15:45:22 +0900

    ㅇㅇ

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var dbs = require('./routes/db');
+//var users = require('./routes/users');
 
 var app = express();
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,20 +27,21 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/db', dbs);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+});*/
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+/*if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -45,17 +49,17 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
-}
+}*/
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+/*app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});
+});*/
 
 
 module.exports = app;
---
+++ routes/db.js
@@ -0,0 +1,46 @@
+﻿var express = require('express');
+var router = express.Router();
+var sql = require('mssql');
+
+var config = {
+    user: 'ocr',
+    password: 'ocr',
+    server: '150.1.4.41',
+    database: 'ocrTest',
+    options: {
+        encrypt: true,
+        database: 'ocrTest'
+    }
+};
+
+var connection = sql.connect(config);
+
+router.post('/insert', function (req, res) {
+    var result = req.body;
+    console.log(result);
+    
+    for (var i = 0; i < result.length; i++) {
+        //insertInfo(result[i].location, result[i].word);
+    }
+});
+
+//좌표, 텍스트 INSERT
+function insertInfo(location, word) {
+    connection.then(function () {
+        var request = new sql.Request();
+        request.query("insert into ocr (ocr_location, ocr_word) values ('" + location + "', '" + word.replace(/'/gi, "''") + "')")
+        //request.query("insert into TBL_OCR (location, word) values ('" + location + "', '" + word.replace(/'/gi, "''") + "')")
+            .then(function (recordset) {
+                //console.log(recordset);
+                //console.log('Affected: ' + request.rowsAffected);
+            }).catch(function (err) {
+                console.log('Request error: ' + err);
+            });
+    }).catch(function (err) {
+        if (err) {
+            console.log('SQL Connection Error: ' + err);
+        }
+    });
+}
+
+module.exports = router;
